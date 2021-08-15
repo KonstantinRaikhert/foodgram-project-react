1f199a03280f9fd635e75ec537716542564cc515
Konstantin Raikhert <konstantinraihert@yandex.ru>
2021-08-16 00:59:19 +0300

    reorganization settings for local and develop

--- backend/configuration/settings/base.py
+++ backend/configuration/settings/base.py
@@ -24,21 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-=3jsy^o^qfdbx(xzje+^y(9-o@od*6c+8vb104=0o8l4bvf5o%"
-)
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    "localhost",
-    "0.0.0.0",
-    "127.0.0.1",
-]
-
-
 # Application definition
 
 DJANGO_APPS = [
--- backend/configuration/wsgi.py
+++ backend/configuration/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configuration.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "configuration.settings.develop"
+)
 
 application = get_wsgi_application()
