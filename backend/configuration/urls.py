38a01613e53bca38f916cc8c8aed15407961cd91
Konstantin Raikhert <konstantinraihert@yandex.ru>
2021-09-07 01:35:04 +0300

    delete DdTb from develop

--- backend/configuration/urls.py
+++ backend/configuration/urls.py
@@ -35,5 +35,5 @@ if DEBUG:
     import debug_toolbar
 
     urlpatterns += [
-        path("__debug__", include(debug_toolbar.urls)),
+        path("__debug__/", include(debug_toolbar.urls)),
     ]
