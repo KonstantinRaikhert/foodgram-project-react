7bfa34a2425442b4d19131e6f757709fd26e5828
Konstantin Raikhert <konstantinraihert@yandex.ru>
2021-10-11 23:07:30 +0300

    add www path

--- backend/configuration/settings/prod.py
+++ backend/configuration/settings/prod.py
@@ -13,7 +13,12 @@ DEBUG = False
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["127.0.0.1", "84.252.134.12", "foodgram.fun"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "84.252.134.12",
+    "foodgram.fun",
+    "www.foodgram.fun",
+]
 
 # Databases
 # ------------------------------------------------------------------------------
