d56fcdfd82ee27a71cec950722f49d2b2276b413
Konstantin Raikhert <konstantinraihert@yandex.ru>
2021-10-11 22:44:08 +0300

    domain name

--- backend/configuration/settings/prod.py
+++ backend/configuration/settings/prod.py
@@ -13,7 +13,7 @@ DEBUG = False
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["*", "127.0.0.1", "84.252.134.12"]
+ALLOWED_HOSTS = ["127.0.0.1", "84.252.134.12", "foodgram.fun"]
 
 # Databases
 # ------------------------------------------------------------------------------
